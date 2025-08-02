bee63751fa10029bba8d40eff1cf5873f1a1fdd1
Yoni <yonirozental@gmail.com>
2025-08-02 20:15:48 +0300

    Add Hilt integration and application class setup

--- app/src/main/java/com/yoni/nanitapp/MainActivity.kt
+++ app/src/main/java/com/yoni/nanitapp/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.yoni.nanitapp.navigation.BirthdayNavigation
 import com.yoni.nanitapp.ui.theme.NanitAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
